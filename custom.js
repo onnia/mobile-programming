f26264c9a96712f4c5a8c25d2ccf7195da8c6a74
Onni Aaltonen <onni.aaltonen@activearkjwt.com>
2015-10-18 18:08:36 +0300

    add speeding up function

--- custom.js
+++ custom.js
@@ -1,101 +1,180 @@
 
 $( document ).ready(function() {
-    console.log( "document loaded" );
 
-
-
-// startgame();
-//addactiveclass();
+    startgame();
+    //addactiveclass();
     hitabutton();
-    updatecounter();
-    time_spent();
+    // updatecounter();
+    //  timer();
+
+
+
+
+
 });
+
+
+function intervalcounter() {
+
+    var counter = 1000;
+    var myFunction = function () {
+        clearInterval(interval);
+        counter *= 0.999;
+        interval = setInterval(myFunction, counter);
+        console.log(counter);
+        $('#interval').html(counter);
+        updatecounter();
+        hitabutton();
+    }
+    var interval = setInterval(myFunction, counter);
+
+}
+
+
+/* TODO:
+ * Create a timer
+ * check
+ * Create a Start for timer
+ * check
+ * Create a Pause for timer
+ * not check
+ * When a timer starts active class is added
+ * check
+ * when button is clicked clicking period slows down
+  *
+  * Create a function that returns a click event of active button
+  *
+  *
+  * */
 
 
 
 function startgame (){
-    start = $('.start-game');
+
+
+    var pause = $('#pause-game');
+    var start = $('#start-game');
+
     start.click(function(e){
         e.preventDefault();
-        console.log("Start game");
-        start.toggleClass('pause');
-        //start.toggleText('Pause');
-        timer();
-        addactiveclass();
+        // pause.removeClass('hide');
+        start.addClass('hide');
+       // console.log("Start game");
+        // randomselect();
+        time_spent();
+        // pausegame();
+
+        hitabutton();
+        intervalcounter();
+
 
     });
 }
 
-function addactiveclass (time){
-   circle = $('.circle');
-   num = (Math.floor(Math.random()*4))+1;
+function pausegame(){
+    var pause = $('#pause-game');
+    var start = $('#start-game');
+    pause.click(function(e){
+        e.preventDefault();
+        start.removeClass('hide')
+        pause.addClass('hide');
+    });
+}
 
-
-    $('.circle:nth-child('+ num +')').toggleClass("active");
+function randomselect (){
+    var num = (Math.floor(Math.random()*4))+1;
+    //  $('.circle:nth-child('+ num +')').toggleClass("active");
 
     if (num == 1) {
-        buttonid = green
+        buttonid = 'green';
     } else if (num == 2) {
-        buttonid = orange
+        buttonid = 'orange';
     } else if (num == 3) {
-        buttonid = red
+        buttonid = 'red';
     } else if (num == 4) {
-        buttonid = yellow
+        buttonid = 'yellow';
     }
+    var buttonidselector = $('#'+buttonid+'');
 
-    //console.log(buttonid);
 
-    hitabutton();
+    // Add active class to the selected id
+    buttonidselector.addClass("active");
 
+    console.log('Selected button '+ buttonidselector.selector);
+
+    return buttonidselector.selector;
 }
 
 function hitabutton(){
-    circle = $('.active');
+    var circle = randomselect();
 
-    if ( ( ".active" ) ) {
+    //save value to variable and create selector
+    var id = $(circle);
 
+    console.log(id);
+
+    // if active id button is clicked
+    if (id.hasClass('active')) {
+        id.on("click", function (e) {
+            var click = true;
+            e.preventDefault();
+
+            console.log('clicked button ' + id);
+            // Add new count
+            updatecounter();
+
+            // Remove Active class from siblings
+            $(this).siblings().removeClass('active');
+            $(this).removeClass('active');
+
+            $(this).delay(100).queue(function () {
+            });
+
+
+            // Call again new button
+            nextselection = randomselect();
+            console.log('Next button is ' + nextselection);
+
+        });
     }
-    circle.click(function(e){
-        e.preventDefault();
-
-        var clickElement = e.target;  // get the dom element clicked.
-        var elementClassName = e.target.className;  // get the classname of the element clicked
-        var theClass = this.className;  // "this" is the element clicked
-        //console.log( elementClassName );
-        //console.log( clickElement );
-        circle.removeClass('active');
-        addactiveclass();
-    });
-
 }
 
 function timer(){
-    setInterval(addactiveclass(), 4000);
+    function timegoesby(){
+        $(".button").click()
+    }
 
-   // console.log(speedup());
+    setTimeout(function() {
+        timegoesby();
+    }, 7000);
+
+
+
+    hitabutton();
 }
+
 
 
 function speedup(){
     setInterval(function(){
-    orginaltime = 4000;
+        orginaltime = 4000;
     },10000)
 
     orginaltime = 4000;
-   // console.log(orginaltime);
+    // console.log(orginaltime);
     //return orginaltime;
 }
 
 function updatecounter(){
-  //  var elementClassName = e.target.className;
-  //  console.log(elementClassName);
-
-    $('.active').click(function() {
-       // .preventDefault();
-        $('#countert').html(function(i, val) { return val*1+1 });
-    });
+    $('#count-amount').html(function(i, val) {
+           // console.log("clicked " + val);
+            return (val*1)+1
+        }
+    );
 }
 
 function time_spent(){
-    $('#timespent').runner('start');
+    var d = new Date();
+    d.setMinutes(d.getMinutes());
+    $('#timer').tinyTimer({ from: d });
 }
